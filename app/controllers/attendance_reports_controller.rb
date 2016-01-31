335dc68a5f74797d24c3d0ec043edc005a06adeb
stevievines <stevievines@gmail.com>
2016-01-31 16:59:48 -0500

    Update Attendance Percentage Rounding

--- app/controllers/attendance_reports_controller.rb
+++ app/controllers/attendance_reports_controller.rb
@@ -26,7 +26,7 @@ class AttendanceReportsController < ApplicationController
         @team.practices.joins(:diver_practices)
           .select("practices.*, (sum(case when diver_practices.was_present then 1 else 0 end) / count(diver_practices.id)::float) as attendance_percentage")
           .order(:date).group("practices.id")
-          .each { |practice| results[practice.date.strftime('%a %m/%d')] = 100 * practice.attendance_percentage.round(2) }
+          .each { |practice| results[practice.date.strftime('%a %m/%d')] = (100 * practice.attendance_percentage).round(0) }
       end
     else
       {}
