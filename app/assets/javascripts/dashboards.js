1684fc2a430f81ddcdc7e08b4be98d32dd3d3200
stevievines <stevievines@gmail.com>
2016-05-31 22:05:56 -0400

    Use select2 for dive selection in goal modal

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $('.js-dive-select').select2({
+    placeholder: "Choose a dive",
+    allowClear: true,
+    width: '100%'
+  });
+
   $('#new-goal-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Button that triggered the modal
     // Extract info from data-* attributes
@@ -6,8 +12,7 @@ $(document).ready(function() {
     var diverId = button.data('diver-id');
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     var modal = $(this);
-    modal.find('.modal-title').text('How many minutes late was ' + diver + '?');
+    modal.find('.modal-title').text('Add a goal for ' + diver);
     modal.find('#diver_id').val(diverId);
   });
-
 });
