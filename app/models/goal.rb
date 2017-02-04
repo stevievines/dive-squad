8093d1e7f2bc9a6fac076e297d16fef4fd03ae9c
stevievines <stevievines@gmail.com>
2017-02-04 15:38:31 -0500

    Show goals on diver profiles

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,11 @@
 class Goal < ActiveRecord::Base
   belongs_to :dive
 
-  delegate :display_description, to: :dive
+  delegate :display_description, :number, to: :dive
 
   validates_uniqueness_of :dive
+
+  scope :three_meter, -> { includes(:dive).where(dives: { height: 3 }) }
+  scope :one_meter, -> { includes(:dive).where(dives: { height: 3 }) }
+  scope :platform, -> { includes(:dive).where(dives: { height: [5, 7, 10] }) }
 end
